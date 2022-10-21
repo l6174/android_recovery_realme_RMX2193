69d170359bfe45160eddb3329cc50f9c4e234341
L Lawliet <114310783+l6174@users.noreply.github.com>
2022-10-22 00:36:41 +0530

    Fixed fastbootd

--- twrp_RMX2193.mk
+++ twrp_RMX2193.mk
@@ -34,4 +34,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
